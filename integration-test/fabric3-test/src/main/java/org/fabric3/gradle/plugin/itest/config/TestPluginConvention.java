e2dec28cb768d3235d87f50530ccf288215a502f
jmarino <jim.marino@gmail.com>
2014-11-13 18:52:39 +0100

    prepare 2.5.0 release

--- integration-test/fabric3-test/src/main/java/org/fabric3/gradle/plugin/itest/config/TestPluginConvention.java
+++ integration-test/fabric3-test/src/main/java/org/fabric3/gradle/plugin/itest/config/TestPluginConvention.java
@@ -35,7 +35,7 @@ public class TestPluginConvention {
     private String systemConfig;
     private File systemConfigFile;
 
-    private String runtimeVersion = "2.5.0-SNAPSHOT";
+    private String runtimeVersion = "2.5.0";
     private String compositeNamespace = "urn:fabric3.org";
     private String compositeName = "TestComposite";
     private String errorText;
